54b9994f77f43ba9ff44538bcbd02ae459714cd0
pglibrary80 <pglibrary80@gmail.com>
2025-02-18 21:20:37 +0900

    repo mocking

---
+++ src/users/providers/create-user.provider.spec.ts
@@ -0,0 +1,76 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { CreateUserProvider } from './create-user.provider';
+import { HashingProvider } from 'src/auth/providers/hashing.provider';
+import { DataSource, ObjectLiteral, Repository } from 'typeorm';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { User } from '../user.entity';
+
+type MockRepository<T extends ObjectLiteral = any> = Partial<
+  Record<keyof Repository<T>, jest.Mock>
+>;
+
+const createMockRepository = <
+  T extends ObjectLiteral = any,
+>(): MockRepository<T> => ({
+  findOne: jest.fn(),
+  create: jest.fn(),
+  save: jest.fn(),
+});
+
+describe('UsersService', () => {
+  let provider: CreateUserProvider;
+  let usersRepository: MockRepository;
+  const user = {
+    firstName: 'John',
+    lastName: 'Doe',
+    email: 'John@Doe.com',
+    password: 'password',
+  };
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        CreateUserProvider,
+        {
+          provide: HashingProvider,
+          useValue: {
+            hashPassword: jest.fn(() => user.password),
+          },
+        },
+        { provide: DataSource, useValue: {} },
+        { provide: getRepositoryToken(User), useValue: createMockRepository() },
+      ],
+    }).compile();
+
+    provider = module.get<CreateUserProvider>(CreateUserProvider);
+    usersRepository = module.get(getRepositoryToken(User));
+  });
+
+  it('should be defined', () => {
+    expect(provider).toBeDefined();
+  });
+
+  it('should be defined', () => {
+    expect(usersRepository).toBeDefined();
+  });
+
+  describe('createUser', () => {
+    describe('When the user does not exist in database', () => {
+      it('should create a new user', async () => {
+        usersRepository.findOne?.mockReturnValue(null);
+        usersRepository.create?.mockReturnValue(user);
+        usersRepository.save?.mockReturnValue(user);
+        const newUser = await provider.createUser(user);
+        expect(usersRepository.findOne).toHaveBeenCalledWith({
+          where: { email: user.email },
+        });
+        expect(usersRepository.create).toHaveBeenCalledWith(user);
+        expect(usersRepository.save).toHaveBeenCalledWith(user);
+      });
+    });
+
+    describe('When the user exist', () => {
+      it('throw BadRequestException', () => {});
+    });
+  });
+});
