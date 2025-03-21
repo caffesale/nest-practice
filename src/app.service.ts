bbdc9695d636918c5cb450149885256ffde9800c
pglibrary80 <pglibrary80@gmail.com>
2025-03-21 20:33:36 +0900

    .

--- src/app.service.ts
+++ src/app.service.ts
@@ -5,4 +5,11 @@ export class AppService {
   getHello(): string {
     return 'Hello From NestJs!';
   }
+
+  /**
+   * This is a simple method that returns a string.
+   */
+  Hello(): string {
+    return 'Hello From NestJs!';
+  }
 }
